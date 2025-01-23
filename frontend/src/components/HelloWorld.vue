6643463749a8a852d63b65ed97a809e3b5faeefa
Andrew <andrey.ermolenko@sibers.com>
2025-01-23 20:56:17 +0700

    message777

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ message: string }>()
+defineProps<{ message777: string }>()
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <h1>{{ message777 }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
