86dfe392acfc8a8eb5e95a7e8b232e32b9fcd5cc
Andrew <andrey.ermolenko@sibers.com>
2025-01-23 21:05:26 +0700

    message222
    # Conflicts:
    #	frontend/src/components/HelloWorld.vue message222

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -6,13 +6,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ message111: string }>()
+defineProps<{ message222: string }>()
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ message777 }}</h1>
+  <h1>{{ message222 }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
